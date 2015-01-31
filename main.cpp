ff99aeb608c0ef7deb756a043c29515984e421b6
Max Yankov <golergka@gmail.com>
2015-01-31 21:18:16 +0200

    More advanced line drawing

--- main.cpp
+++ main.cpp
@@ -2,18 +2,25 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green = TGAColor(0,   255, 0,   255);
+const TGAColor blue	 = TGAColor(0,   0,   255, 255);
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-	for(float t=0.; t<1.; t+=.01) {
-		int x = x0*(1.-t) + x1*t;
-		int y = y0*(1.-t) + y1*t;
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+	for(int x = x0; x <= x1; x++)
+	{
+		float t = (x - x0)/(float) (x1 - x0);
+		int y = y0 * (1. - t) + y1 * t;
 		image.set(x, y, color);
 	}
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
 	TGAImage image(100, 100, TGAImage::RGB);
-	line(3,5,17,48,image,red);
+	line(13, 20, 80, 40, image, white);
+	line(20, 13, 40, 80, image, red);
+	line(80, 40, 13, 20, image, green);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;
