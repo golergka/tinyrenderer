729ead97d7d353f70af83e122a68edcd6bd32743
Max Yankov <golergka@gmail.com>
2015-01-31 21:20:55 +0200

    Fixed lines

--- main.cpp
+++ main.cpp
@@ -7,11 +7,30 @@ const TGAColor blue	 = TGAColor(0,   0,   255, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
+	bool steep = false;
+	if (std::abs(x0 - x1) < std::abs(y0 - y1))
+	{
+		std::swap(x0, y0);
+		std::swap(x1, y1);
+		steep = true;
+	}
+	if (x0 > x1)
+	{
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
 	for(int x = x0; x <= x1; x++)
 	{
 		float t = (x - x0)/(float) (x1 - x0);
 		int y = y0 * (1. - t) + y1 * t;
-		image.set(x, y, color);
+		if (steep)
+		{
+			image.set(y, x, color);
+		}
+		else
+		{
+			image.set(x, y, color);
+		}
 	}
 }
 
