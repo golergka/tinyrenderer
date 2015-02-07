48f71e4ab6b6a680d2e944ac2ae65b103b8624ce
Max Yankov <golergka@gmail.com>
2015-02-07 13:26:10 +0200

    Triangles

--- main.cpp
+++ main.cpp
@@ -12,26 +12,30 @@ Model *model = NULL;
 const int width		= 800;
 const int height	= 800;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+void line(
+		Vec2i		t0, 
+		Vec2i		t1, 
+		TGAImage&	image, 
+		TGAColor	color
+	)
 {
 	bool steep = false;
-	if (std::abs(x0 - x1) < std::abs(y0 - y1))
+	if (std::abs(t0.x - t1.x) < std::abs(t0.y - t1.y))
 	{
-		std::swap(x0, y0);
-		std::swap(x1, y1);
+		std::swap(t0.x, t0.y);
+		std::swap(t1.x, t1.y);
 		steep = true;
 	}
-	if (x0 > x1)
+	if (t0.x > t1.x)
 	{
-		std::swap(x0, x1);
-		std::swap(y0, y1);
+		std::swap(t0, t1);
 	}
-	int dx = x1 - x0;
-	int dy = y1 - y0;
+	int dx = t1.x - t0.x;
+	int dy = t1.y - t0.y;
 	int derror2 = std::abs(dy)*2;
 	int error2 = 0;
-	int y = y0;
-	for(int x = x0; x <= x1; x++)
+	int y = t0.y;
+	for(int x = t0.x; x <= t1.x; x++)
 	{
 		if (steep)
 		{
@@ -44,10 +48,43 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 		error2 += derror2;
 		if (error2 > dx)
 		{
-			y += ( y1 > y0 ? 1 : -1);
-			error2 -= dx*2;
+			y += (t1.y > t0.y ? 1 : -1);
+			error2 -= dx * 2;
 		}
 	}
+}
+
+void triangle(
+		Vec2i		t0,
+		Vec2i		t1,
+		Vec2i		t2,
+		TGAImage&	image,
+		TGAColor	color
+	)
+{
+	if (t0.y > t1.y)
+	{
+		std::swap(t0, t1);
+	}
+	if (t0.y > t2.y)
+	{
+		std::swap(t0, t2);
+	}
+	if (t1.y > t2.y)
+	{
+		std::swap(t1, t2);
+	}
+	line(t0, t1, image, color);
+	line(t1, t2, image, color);
+	line(t0, t2, image, color);
+}
+
+Vec2i to_screen_space(Vec3f v)
+{
+	return Vec2i(
+			(v.x + 1.) * (width / 2.),
+			(v.y + 1.) * (height / 2.)
+		);
 }
 
 int main(int argc, char** argv)
@@ -64,16 +101,16 @@ int main(int argc, char** argv)
 	for (int i = 0; i < model->nfaces(); i++)
 	{
 		std::vector<int> face = model->face(i);
-		for(int j = 0; j < 3; j++)
-		{
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j+1)%3]);
-			int x0 = (v0.x + 1.) * width / 2.;
-			int y0 = (v0.y + 1.) * height / 2.;
-			int x1 = (v1.x + 1.) * width / 2.;
-			int y1 = (v1.y + 1.) * height / 2.;
-			line(x0, y0, x1, y1, image, white);
-		}
+		Vec3f v0 = model->vert(face[0]);
+		Vec3f v1 = model->vert(face[1]);
+		Vec3f v2 = model->vert(face[2]);
+		triangle(
+				to_screen_space(v0),
+				to_screen_space(v1),
+				to_screen_space(v2),
+				image,
+				white
+			);
 	}
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
