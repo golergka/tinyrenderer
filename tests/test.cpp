0f0a38175df244a41e3fab1322316eb232a5ed2d
Max Yankov <golergka@Aperture-Science-Mac.local>
2015-02-14 14:36:30 +0200

    Included Catch unit testing framework with test assertions

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
-int main(int argc, char** argv)
+unsigned int Factorial(unsigned int number)
 {
-	std::cerr << "Tests complete";
+	return number <= 1 ? number : Factorial(number - 1) * number;
+}
+
+TEST_CASE("Factorials are computer", "[factorial]")
+{
+	REQUIRE( Factorial(1) == 1);
+	REQUIRE( Factorial(2) == 2);
+	REQUIRE( Factorial(3) == 6);
+	REQUIRE( Factorial(10) == 3628800);
 }
